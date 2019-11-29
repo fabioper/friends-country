41a1b27488df2d488893895349f8a2ac6d29c0a5
Fábio Pereira <fabiopereira.fps@gmail.com>
2019-11-29 01:26:13 -0300

    Add base solutionand files

---
+++ FriendsCountry/FriendsCountry.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using FriendsCountry.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FriendsCountry.Domain.Interfaces
+{
+    interface IRepository<T> where T: Entity
+    {
+    }
+}
